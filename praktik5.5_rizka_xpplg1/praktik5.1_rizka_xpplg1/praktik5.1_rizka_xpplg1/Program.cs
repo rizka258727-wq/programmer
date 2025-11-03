b1059ff26c74dec7968e40e93d8097eb75531a05
rizka258727-wq <rizka258727@student.smkn1kandeman.sch.id>
2025-11-03 14:29:03 +0700

    Add files via upload

---
+++ praktik5.5_rizka_xpplg1/praktik5.1_rizka_xpplg1/praktik5.1_rizka_xpplg1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik5._1_rizka_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perulangan untuk menampilkan angka 1 sampai 10:");
+
+            // Perulangan for dimulai di sini for
+            for (int i = 1; i <= 10; i++)
+            {
+                // Blok kode yang akan diulang
+                Console.WriteLine("Angka ke-" + i);
+            }
+
+            Console.WriteLine("\nPerulangan selesai.");
+        }
+    }
+}
