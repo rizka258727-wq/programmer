1ba3b97d013841572f943e4a0111221b584aa19b
rizka258727-wq <rizka258727@student.smkn1kandeman.sch.id>
2025-11-03 14:27:32 +0700

    Add files via upload

---
+++ praktik5.3_rizka_xpplg1/praktik5.3_rizka_xpplg1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik5._3_rizka_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inisialisasi variabel untuk menyimpan input
+            string inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hingga Anda mengetik 'keluar'.");
+
+            // Perulangan selama isi variabel inputUser TIDAK SAMA DENGAN "keluar"
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.Write("\nKetik sesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine(); // Baca input dari user
+
+                Console.WriteLine("Anda mengetik: " + inputUser);
+            }
+
+            Console.WriteLine("\nProgram selesai. Terima kasih!");
+        }
+
+        private static void ReadLine()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
