ea9cc491dd7285f3e261d959c12f63fc5554dfbd
rizka258727-wq <rizka258727@student.smkn1kandeman.sch.id>
2025-11-03 14:15:08 +0700

    Add files via upload

---
+++ praktik4.8_rizka_xpplg1/praktik4.8_rizka_xpplg1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik4._8_rizka_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka: ");
+            int angka = int.Parse(Console.ReadLine());
+
+            if (angka > 0) 
+            {
+                Console.WriteLine("Angka Positif.");
+            }
+            else if (angka < 0)
+            {
+                Console.WriteLine("Angka Negatif.");
+            }
+            else
+            {
+                Console.WriteLine("Angka Nol.");
+            }
+        }
+    }
+}
---
+++ praktik4.9_rizka_xpplg1/praktik4.9_rizka_xpplg1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik4._9_rizka_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan umur: ");
+            int umur = int.Parse(Console.ReadLine());
+
+            if (umur < 5)
+            {
+            else if (umur <= 60)             }Tiket
+
+                Console.WriteLine("Harga Tiket: Rp 50.000");
+            { 
+                Console.WriteLine(" Gratis.");
+            }
+            else if (umur <= 12)
+            {
+                Console.WriteLine("Harga Tiket: Rp 20.000");
+            }
+            else
+            {
+                Console.WriteLine("Harga Tiket: Rp 30.000 (Diskon Lansia)");
+            }
+        }
+    }
+}
